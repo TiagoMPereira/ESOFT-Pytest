f41d261047a33061d866986e002de9eb4caeec24
Tiago Pereira <tiagodemoraispereira@gmail.com>
2022-09-27 19:44:16 -0300

    ADDED: Funcionalidade para mover elevador

--- source/elevador/elevador.py
+++ source/elevador/elevador.py
@@ -5,7 +5,7 @@ class Elevador:
         self.andar_maximo = 48
         self.andar_minimo = -2 # Subsolos
 
-    def mover(self, andar):
+    def mover_um_andar(self, andar):
         if andar > self.andar_atual:
             self.movimento = 1
         elif andar < self.andar_atual:
@@ -14,3 +14,8 @@ class Elevador:
             self.movimento = 0
 
         self.andar_atual += self.movimento
+
+    def mover(self, andar):
+        while self.andar_atual != andar:
+            self.mover_um_andar(andar)
+        self.movimento = 0
--- test/test_elevador/test_elevador.py
+++ test/test_elevador/test_elevador.py
@@ -14,9 +14,30 @@ class TestElevador:
     def test_elevador_andar_inicial(self):
         assert self.elevador.andar_atual == 0
 
-    def test_elevador_mover(self):
+    def test_elevador_mover_um_andar(self):
         andar_desejado = 1
+
+        self.elevador.mover_um_andar(andar=andar_desejado)
+
+        assert self.elevador.movimento != 0
+
+    def test_elevador_mover_andar_final_subir(self):
+        andar_desejado = 10
 
         self.elevador.mover(andar=andar_desejado)
 
-        assert self.elevador.movimento != 0
+        assert self.elevador.andar_atual == andar_desejado
+    
+    def test_elevador_mover_andar_final_descer(self):
+        andar_desejado = -1
+
+        self.elevador.mover(andar=andar_desejado)
+
+        assert self.elevador.andar_atual == andar_desejado
+    
+    def test_elevador_mover_andar_final_parado(self):
+        andar_desejado = -1
+
+        self.elevador.mover(andar=andar_desejado)
+
+        assert self.elevador.movimento == 0
