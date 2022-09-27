b7caadaba44c425af44152265f0e4e1f41006d89
Tiago Pereira <tiagodemoraispereira@gmail.com>
2022-09-27 19:36:27 -0300

    ADDED: Funcionalidade para mover elevador

--- source/elevador/elevador.py
+++ source/elevador/elevador.py
@@ -4,3 +4,13 @@ class Elevador:
         self.andar_atual = 0 # Térreo
         self.andar_maximo = 48
         self.andar_minimo = -2 # Subsolos
+
+    def mover(self, andar):
+        if andar > self.andar_atual:
+            self.movimento = 1
+        elif andar < self.andar_atual:
+            self.movimento = -1
+        else:
+            self.movimento = 0
+
+        self.andar_atual += self.movimento
