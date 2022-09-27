728ab90490b3e43aa820324f6c9ca5631ab6fb0a
Izaltino Romao Neto <izaltinoromaoneto@hotmail.com>
2022-09-27 19:50:05 -0300

    ADDED: teste criando um usuario com id

---
+++ test/test_usuario/test_usuario.py
@@ -0,0 +1,12 @@
+import pytest
+
+from source.usuario.usuario import Usuario
+
+class TestUsuario:
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.usuario = Usuario(id = 123456)
+
+    def test_cria_usuario_com_id(self):
+        assert self.usuario.id = 123456
